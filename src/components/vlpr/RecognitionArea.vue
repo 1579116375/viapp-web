93a4de3989b5cc22de843adf1882ae42a8fe9981
napcxy <napcxy@outlook.com>
2022-07-18 17:56:12 +0800

    improve vlpr page

---
+++ src/components/vlpr/RecognitionArea.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import SplitDataImg from '@/assets/images/split/split-01.jpg'
+</script>
+
+<template>
+  <div class="rn-splite-style">
+    <div class="split-wrapper">
+      <div class="row no-gutters radius-10 align-items-center">
+        <div class="col-lg-12 col-xl-6 col-12">
+          <div class="thumbnail image-left-content">
+            <img :src="SplitDataImg" alt="Split Image" />
+          </div>
+        </div>
+        <div class="col-lg-12 col-xl-6 col-12">
+          <div class="split-inner">
+            <h4 class="title">识别信息</h4>
+            <p class="description">
+              识别中国大陆各类机动车车牌信息，支持蓝牌、黄牌（单双行）、绿牌、大型新能源（黄绿）、领使馆车牌、警牌、武警牌（单双行）、军牌（单双行）、港澳出入境车牌、农用车牌、民航车牌，并能同时识别图像中的多张车牌
+            </p>
+            <ul class="split-list">
+              <!-- <li v-for="listItem in splitData.list">{{ listItem }}</li> -->
+            </ul>
+            <div class="view-more-button mt--35">
+              <!-- <Button :title="splitData.btn.text" :url="splitData.btn.link" /> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/vlpr/index.vue
+++ src/views/vlpr/index.vue
@@ -1,6 +1,26 @@
 <script lang="ts" setup>
+import AppLayout from '@/components/AppLayout.vue'
+import SectionTitle from '@/components/intro/SectionTitle.vue'
+import RecognitionArea from '@/components/vlpr/RecognitionArea.vue'
 </script>
 
 <template>
-  <h1>VLPR</h1>
+  <app-layout>
+    <div class="rwt-split-area rn-section-gap">
+      <div class="wrapper">
+        <div class="row">
+          <div class="col-lg-12 mb--40">
+            <section-title
+              text-align="center"
+              subtitle="开发功能"
+              title="车牌识别"
+              data-aos="fade-up"
+              data-aos-delay="60"
+            />
+          </div>
+        </div>
+        <recognition-area />
+      </div>
+    </div>
+  </app-layout>
 </template>
