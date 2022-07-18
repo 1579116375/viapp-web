108395ef72d52917463a4dd097b636d082d31d8b
Shaw <9723100+ShawGectgu@user.noreply.gitee.com>
2022-07-18 17:25:38 +0800

    update main page

--- src/components/global/GlobalNav.vue
+++ src/components/global/GlobalNav.vue
@@ -2,31 +2,45 @@
 import MenuItem, { MenuItemData } from '@/components/global/MenuItem.vue'
 
 const keyFunction: MenuItemData[] = [
-  { routerLink: '/', routerName: '车牌识别' },
-  { routerLink: '/', routerName: '车型识别' },
-  { routerLink: '/', routerName: '车辆检测' },
-  { routerLink: '/', routerName: '车流统计' },
-  { routerLink: '/', routerName: '车辆属性识别' },
-  { routerLink: '/', routerName: '车辆外观损伤识别' }
+  { routerLink: '/', routerName: '图像识别' },
+  { routerLink: '/', routerName: '车辆分析' },
+  { routerLink: '/', routerName: '图像搜索' },
+  { routerLink: '/', routerName: '图像内容安全' },
+  { routerLink: '/', routerName: '图像特效' },
+  { routerLink: '/', routerName: '图像增强' }
 ]
 
 const elemData1: MenuItemData[] = [
-  { routerLink: '/', routerName: '车牌识别' },
-  { routerLink: '/', routerName: '车型识别' },
-  { routerLink: '/', routerName: '车辆检测' },
-  { routerLink: '/', routerName: '车流统计' },
-  { routerLink: '/', routerName: '车辆属性识别' },
-  { routerLink: '/', routerName: '车辆外观损伤识别' },
-  { routerLink: '/', routerName: '车辆分割' }
+  { routerLink: '/', routerName: '通用物体和场景识别别', spanClass: 'rn-badge-card', spanContent: '热门' },
+  { routerLink: '/', routerName: '植物识别' },
+  { routerLink: '/', routerName: '动物识别' },
+  { routerLink: '/', routerName: '地标识别', spanClass: 'rn-badge-card', spanContent: '新品' },
+  { routerLink: '/', routerName: '货币识别' },
+  { routerLink: '/', routerName: '翻拍识别', spanClass: 'rn-badge-card', spanContent: '新品' },
+  { routerLink: '/', routerName: '果蔬识别' }
 ]
 
 const elemData2: MenuItemData[] = [
-  { routerLink: '/', routerName: '车牌识别', spanClass: 'rn-badge-card', spanContent: 'Hot' },
-  { routerLink: '/', routerName: '车牌识别', spanClass: 'rn-badge-card', spanContent: 'Coming' },
-  { routerLink: '/', routerName: '车牌识别', spanClass: 'rn-badge-card', spanContent: 'Hot' },
-  { routerLink: '/', routerName: '车牌识别' },
-  { routerLink: '/', routerName: '车牌识别' }
+  { routerLink: '/', routerName: '车型识别', spanClass: 'rn-badge-card', spanContent: '开发中' },
+  { routerLink: '/', routerName: '车牌识别', spanClass: 'rn-badge-card', spanContent: '热门' },
+  { routerLink: '/', routerName: '车辆属性识别' },
+  { routerLink: '/', routerName: '车辆损伤识别' },
+  { routerLink: '/', routerName: '驾驶员行为分析', spanClass: 'rn-badge-card', spanContent: '热门' }
 ]
+const elemData3: MenuItemData[] = [
+  { routerLink: '/', routerName: '色情识别', spanClass: 'rn-badge-card', spanContent: '邀测' },
+  { routerLink: '/', routerName: '违禁识别', spanClass: 'rn-badge-card', spanContent: '热门' },
+  { routerLink: '/', routerName: '广告检测', spanClass: 'rn-badge-card', spanContent: '热门' },
+  { routerLink: '/', routerName: '公众人物识别' },
+  { routerLink: '/', routerName: '恶心图像识别' }
+]
+const elemData4: MenuItemData[] = [
+  { routerLink: '/', routerName: '黑白图像上色', spanClass: 'rn-badge-card', spanContent: '邀测' },
+  { routerLink: '/', routerName: '图像风格转换' },
+  { routerLink: '/', routerName: '人物动漫化', spanClass: 'rn-badge-card', spanContent: '新品' },
+  { routerLink: '/', routerName: '天空分割', spanClass: 'rn-badge-card', spanContent: '热门' }
+]
+
 </script>
 
 <template>
@@ -48,10 +62,10 @@ const elemData2: MenuItemData[] = [
               <menu-item item-type="mega-menu-item" :data="elemData2" />
             </div>
             <div class="col-lg-3 single-mega-item">
-              <menu-item item-type="mega-menu-item" :data="elemData1" />
+              <menu-item item-type="mega-menu-item" :data="elemData3" />
             </div>
             <div class="col-lg-3 single-mega-item">
-              <menu-item item-type="mega-menu-item" :data="elemData2" />
+              <menu-item item-type="mega-menu-item" :data="elemData4" />
             </div>
           </div>
         </div>
