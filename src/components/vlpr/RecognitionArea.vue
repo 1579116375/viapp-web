00cc4e5b8f42cbdde1e15adf5fa31045c91d4053
napcxy <napcxy@outlook.com>
2022-07-19 10:34:38 +0800

    improve vlpr func

--- src/components/vlpr/RecognitionArea.vue
+++ src/components/vlpr/RecognitionArea.vue
@@ -1,13 +1,29 @@
 <script lang="ts" setup>
-import SplitDataImg from '@/assets/images/split/split-01.jpg'
 import Button from '@/components/common/Button.vue'
 import RetInfo, { RetProps } from '@/components/common/RetInfo.vue'
+import { ref, getCurrentInstance, onMounted, onBeforeUpdate, onUpdated } from 'vue'
+
+import SplitDataImg from '@/assets/images/split/split-01.jpg'
+
+let RecogImg = ref(SplitDataImg)
 
 const list: RetProps[] = [
   { title: '车牌', msg: '京A·888888' },
   { title: '车牌', msg: '京A·888888' },
   { title: '车牌', msg: '京A·888888' }
 ]
+
+const curInstance: any = getCurrentInstance()
+
+function uploadImgTrigger() {
+  curInstance.refs.updRef.dispatchEvent(new MouseEvent('click'))
+}
+
+function uploadImg(event: any) {
+  const file = event.target.files[0]
+  const formData = new FormData()
+  formData.append('file', file)
+}
 </script>
 
 <template>
@@ -15,14 +31,15 @@ const list: RetProps[] = [
     <div class="split-wrapper">
       <div class="row no-gutters radius-10 align-items-center">
         <div class="col-lg-12 col-xl-6 col-12">
-          <div class="thumbnail image-left-content">
-            <img :src="SplitDataImg" alt="Split Image" />
+          <input id="file-upload" ref="updRef" @change="uploadImg" type="file" accept="image/*" name="picture" />
+          <div id="recog-img-con" class="thumbnail image-left-content" @click="uploadImgTrigger">
+            <img :src="RecogImg" alt="Split Image" />
           </div>
         </div>
         <div class="col-lg-12 col-xl-6 col-12">
           <div class="split-inner">
             <p class="description">
-              点击左侧区域上传含车牌的汽车图片，点击“开始识别”按钮开始识别
+              点击图片显示区域上传含车牌的汽车图片或车牌图片，点击“开始识别”按钮开始识别
             </p>
             <div class="rbt-single-widget widget_recent_entries mt--40">
               <h3 class="title">识别结果</h3>
@@ -39,3 +56,13 @@ const list: RetProps[] = [
     </div>
   </div>
 </template>
+
+<style scoped>
+#recog-img-con:hover {
+  cursor: pointer;
+}
+
+#file-upload {
+  display: none;
+}
+</style>
